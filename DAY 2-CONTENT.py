5ba6f7fbed6a2ff8a9d0a93e038e09bb568fa34e
gayathrimummidi <109512103+gayathrimummidi@users.noreply.github.com>
2023-02-02 18:50:03 +0530

    Add files via upload

---
+++ DAY 2-CONTENT.py
@@ -0,0 +1,39 @@
+list and its operations:
+
+list accessing:
+l[0].....l[n]
+
+
+**
+[1, 3, 6.9, 'dog']
+l[0]
+1
+l[2:3]
+[6.9]
+l[0:]
+[1, 3, 6.9, 'dog']
+l[:4]
+[1, 3, 6.9, 'dog']
+l[-1]
+'dog'
+l[::-1]
+['dog', 6.9, 3, 1]
+operations:
+**
+append()- add an element to the end of the list=l.append(n)
+extend()- add all elements of a list to another list=l.extend([5,6])
+insert()-inser an item=l.insert(1,22)-ist index
+remove()-remove an item
+pop()-removes and returns an element at index
+clear()-removes all items
+index()-returns the index
+count()-returns count of an item repeated
+sort()-sorts list in ascendimg order
+reverse()-revers the order of items in the list
+copy()-returns a shallow copy list 
+
+
+#
+1.create a list with 10 no and list them
+2.create a list with 5 float numbers find and display sum and avg of the list
+3.after creating a list with 6 elements from the user extract only even no and print
